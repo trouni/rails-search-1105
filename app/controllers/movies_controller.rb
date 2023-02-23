0c0e799675fa6f51998e9ba584ff53f247dd83f4
Trouni Tiet <trouni@gmail.com>
2023-02-23 10:54:07 +0900

    Add PgSearch

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -4,14 +4,33 @@ class MoviesController < ApplicationController
       # # /movies?query=Batman
       # params[:query]
 
-      sql_query = <<~SQL
-        movies.title @@ :q
-        OR movies.synopsis @@ :q
-        OR directors.first_name @@ :q
-        OR directors.last_name @@ :q
-      SQL
+      # ========
+      # PgSearch
+      # ========
 
-      @movies = Movie.joins(:director).where(sql_query, q: "%#{params[:query]}%")
+      ## Without multisearch
+      @movies = Movie.search(params[:query])
+
+      ## With multisearch
+      # @movies = PgSearch.multisearch(params[:query])
+
+      ## Further filtering of results
+      # @movies = @movies.where('price <= ?', params[:max_price]) if params[:max_price].present?
+      # @movies = @movies.where('price >= ?', params[:min_price]) if params[:min_price].present?
+      # @movies = @movies.near(params[:location], params[:radius] || 5) if params[:location].present?
+
+      # ==================
+      # Plain ActiveRecord
+      # ==================
+
+      # sql_query = <<~SQL
+      #   movies.title @@ :q
+      #   OR movies.synopsis @@ :q
+      #   OR directors.first_name @@ :q
+      #   OR directors.last_name @@ :q
+      # SQL
+
+      # @movies = Movie.joins(:director).where(sql_query, q: "%#{params[:query]}%")
     else
       @movies = Movie.all
     end
--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,3 +1,17 @@
 class Movie < ApplicationRecord
   belongs_to :director
+
+  include PgSearch::Model
+  multisearchable against: [:title, :synopsis]
+
+  # pg_search_scope :name_of_my_search_method
+  pg_search_scope :search,
+    against: [ :title, :synopsis ],
+    associated_against: {
+      director: [ :first_name, :last_name ],
+      # actors: [:first_name, :last_name] # for more tables
+    },
+    using: {
+      tsearch: { prefix: true } # <-- now `superman batm` will return something!
+    }
 end
